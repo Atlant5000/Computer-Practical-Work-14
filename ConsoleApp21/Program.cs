cb43063b6fafb2c1262b1effc5abc5aac4ddf655
Maksim Mihailovskii <maximmmd13@gmail.com>
2024-12-17 09:43:14 +0200

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    /// <summary>
+    /// С использованием цветового оформления консоли:
+    /// а) вывести на экран горизонтальную строку из 18 символов,
+    /// б) вывести на экран вертикальную строку из 25 символов.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Func();
+            Func1();
+            Console.ReadKey();
+        }
+        public static void Func() 
+        {
+            int x = 1;
+            while (x < 19)
+            {
+                Console.Write(" ");
+                x++;
+            }
+        }
+        public static void Func1()
+        {
+            int x = 1;
+            while (x < 26)
+            {
+                Console.WriteLine(" ");
+                x++;
+            }
+        }
+    }
+}
